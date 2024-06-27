3d4f8b1665d3eb0ef343903013846691915065b8
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-06-27 20:29:20 +0000

    created some components and began building basic structure of app

--- src/js/component/Container.jsx
+++ src/js/component/Container.jsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { Clock } from "./Counter";
+import { Clock } from "./Clock";
+import { Counter } from "./Counter";
 
 export const Container = () => {
     return (<div className="row container-fluid holder">
-        <div className="col-2" style={{background:"white", margin:"10px"}}></div>
+        <div className="col-2" style={{background:"black", margin:"10px", border:"1px solid white", borderRadius:"10px"}}>
+            {<Clock/>}
+        </div>
+        <div className="col" style={{background:"black", margin:"10px", border:"1px solid white", borderRadius:"10px"}}>
+            {<Counter/>}
+        </div>
+        <div className="col" style={{background:"black", margin:"10px", border:"1px solid white", borderRadius:"10px"}}>
+            {<Counter/>}
+        </div>
+        <div className="col" style={{background:"black", margin:"10px", border:"1px solid white", borderRadius:"10px"}}>
+            {<Counter/>}
+        </div>
+        <div className="col" style={{background:"black", margin:"10px", border:"1px solid white", borderRadius:"10px"}}>
+            {<Counter/>}
+        </div>
+        <div className="col" style={{background:"black", margin:"10px", border:"1px solid white", borderRadius:"10px"}}>
+            {<Counter/>}
+        </div>
+        
     </div>);
 }
--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export const Clock = () => {
-    <FontAwesomeIcon icon="fa-regular fa-clock" />
-}
+export const Counter = () => {
+    return (
+        <div style={{color:"white", fontSize:"200px", marginTop:"-50px", marginBottom:"-100px", padding:"0", textAlign:"center"}}>0</div>
+    );
+};
+
+
+
