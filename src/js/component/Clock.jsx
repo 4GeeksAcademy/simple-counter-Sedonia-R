4ebb714dfd57341d724330b2be3362305ef18be3
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-06-30 15:05:18 +0000

    completely remade project, following video explanation, using props instead of states

--- src/js/component/Clock.jsx
+++ src/js/component/Clock.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Clock = () => {
     return (
-    <i className="fa-regular fa-clock" style={{fontSize:"170px", marginTop:"15%", color:"white"}}></i>
+    <i className="fa-regular fa-clock"></i>
     );
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,52 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// include your styles into the webpack bundle
+import PropTypes from 'prop-types';
 import "../styles/index.css";
+import { Clock } from './component/Clock'
 
-//import your own components
-import { App } from "./component/App.jsx";
+function Container(props) {
+    return (<div className="row container-fluid holder">
+        <div className="col-2 clockIcon">
+            {<Clock/>}
+        </div>
+        <div className="col numberCounter five">
+            {props.fifthDigit % 10}
+        </div>
+        <div className="col numberCounter four">
+            {props.fourthDigit % 10}
+        </div>
+        <div className="col numberCounter three">
+            {props.thirdDigit % 10}
+        </div>
+        <div className="col numberCounter two">
+            {props.secondDigit % 10}
+        </div>
+        <div className="col numberCounter one">
+            {props.firstDigit % 10}
+        </div>
+    </div>);
+}
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<App/>);
+Container.propTypes = {
+    fifthDigit: PropTypes.number,
+    fourthDigit: PropTypes.number,
+    thirdDigit: PropTypes.number,
+    secondDigit: PropTypes.number,
+    firstDigit: PropTypes.number
+};
 
+let counter = 0;
+setInterval(function() {
+    const five = Math.floor(counter/10000);
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter);
+    counter++;
+    root.render(
+        <Container firstDigit={one} secondDigit={two} thirdDigit={three} 
+        fourthDigit={four} fifthDigit={five}/>
+    )
+}, 1000);
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
